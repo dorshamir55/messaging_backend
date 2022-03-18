3844554dd2781567df34a7002615692f26664dac
Dor <dor@spectory.com>
2022-03-18 15:33:29 +0200

    init: update gitignore file, Message model, configuring postgres db, running migrations

--- messaging_app/message/models.py
+++ messaging_app/message/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    sender = models.CharField(max_length=255, null=False)
+    recipient = models.CharField(max_length=255, null=False)
+    message = models.TextField(max_length=2000)
+
+    class Meta:
+        db_table = "message"
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'messaging_app',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
